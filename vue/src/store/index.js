0c61e0cb4173a3d419341868d29b38190823ce05
Guy Koumba <guykoumba@gmail.com>
2022-07-18 05:58:47 +0000

    initial

---
+++ vue/src/store/index.js
@@ -0,0 +1,11 @@
+import {createStore} from "vuex";
+
+const store = createStore({
+    state: {
+        user: {
+            data: {name: 'Zura'}, token: null
+        }
+    }, getters: {}, actions: {}, mutations: {}, modules: {}
+})
+
+export default store;
