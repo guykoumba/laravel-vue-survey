553574a4fb43457e826abf1ebe257a27e7df0f90
Guy Koumba <guykoumba@gmail.com>
2022-07-20 22:01:10 +0000

    logout on frontend 1

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -79,7 +79,7 @@ class AuthController extends Controller
 
         return response([
                 'success' => true
-            ] );
+            ]);
 
     }
 }
--- vue/src/components/DefaultLayout.vue
+++ vue/src/components/DefaultLayout.vue
@@ -5,48 +5,47 @@
                 <div class="flex items-center justify-between h-16">
                     <div class="flex items-center">
                         <div class="flex-shrink-0">
-                            <img class="h-8 w-8" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow" />
+                            <img class="h-8 w-8" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                                alt="Workflow" />
                         </div>
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
-                                <router-link 
-                                v-for="item in navigation" 
-                                :key="item.name" 
-                                :to="item.to"
-                                active-class= "bg-gray-900 text-white"
-                                :class="[
-                                    this.$route.name === item.to.name
-                                    ? '' 
-                                    : 'text-gray-300 hover:bg-gray-700 hover:text-white', 
-                                    'px-3 py-2 rounded-md text-sm font-medium'
-                                ]" 
-                                aria-current="item.current ? 'page' : undefined">
-                                {{ item.name }}
+                                <router-link v-for="item in navigation" :key="item.name" :to="item.to"
+                                    active-class="bg-gray-900 text-white" :class="[
+                                        this.$route.name === item.to.name
+                                            ? ''
+                                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                        'px-3 py-2 rounded-md text-sm font-medium'
+                                    ]" aria-current="item.current ? 'page' : undefined">
+                                    {{ item.name }}
                                 </router-link>
                             </div>
                         </div>
                     </div>
                     <div class="hidden md:block">
-                        <div class="ml-4 flex items-center md:ml-6">              
+                        <div class="ml-4 flex items-center md:ml-6">
 
                             <!-- Profile dropdown -->
                             <Menu as="div" class="ml-3 relative">
                                 <div>
-                                    <MenuButton class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                                    <MenuButton
+                                        class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                                         <span class="sr-only">Open user menu</span>
                                         <img class="h-8 w-8 rounded-full" :src="user.imageUrl" alt="" />
                                     </MenuButton>
                                 </div>
-                                <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-                                    <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                        <MenuItem 
-                                          v-slot="{ active }">
-                                            <a 
-                                            @click="logout" 
-                                            :class="[
-                                                'block px-4 py-2 text-sm text-gray-700 cursor-pointer'
-                                            ]"
-                                            >Sign out</a>
+                                <transition enter-active-class="transition ease-out duration-100"
+                                    enter-from-class="transform opacity-0 scale-95"
+                                    enter-to-class="transform opacity-100 scale-100"
+                                    leave-active-class="transition ease-in duration-75"
+                                    leave-from-class="transform opacity-100 scale-100"
+                                    leave-to-class="transform opacity-0 scale-95">
+                                    <MenuItems
+                                        class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                        <MenuItem v-slot="{ active }">
+                                        <a @click="logout" :class="[
+                                            'block px-4 py-2 text-sm text-gray-700 cursor-pointer'
+                                        ]">Sign out</a>
                                         </MenuItem>
                                     </MenuItems>
                                 </transition>
@@ -55,7 +54,8 @@
                     </div>
                     <div class="-mr-2 flex md:hidden">
                         <!-- Mobile menu button -->
-                        <DisclosureButton class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                        <DisclosureButton
+                            class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                             <span class="sr-only">Open main menu</span>
                             <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
                             <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
@@ -66,19 +66,14 @@
 
             <DisclosurePanel class="md:hidden">
                 <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                    <router-link 
-                    v-for="item in navigation"
-                    :key="item.name" as="a" 
-                    :to="item.to"
-                    active-class="bg-gray-900 text-white"
-                    :class="[
-                        this.$route.name === item.to.name
-                        ? '' 
-                        : 'text-gray-300 hover:bg-gray-700 hover:text-white', 
-                        'block px-3 py-2 rounded-md text-base font-medium'
-                    ]" 
-                    >
-                    {{ item.name }}
+                    <router-link v-for="item in navigation" :key="item.name" as="a" :to="item.to"
+                        active-class="bg-gray-900 text-white" :class="[
+                            this.$route.name === item.to.name
+                                ? ''
+                                : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                            'block px-3 py-2 rounded-md text-base font-medium'
+                        ]">
+                        {{ item.name }}
                     </router-link>
                 </div>
                 <div class="pt-4 pb-3 border-t border-gray-700">
@@ -92,15 +87,11 @@
                         </div>
                     </div>
                     <div class="mt-3 px-2 space-y-1">
-                        <DisclosureButton                        
-                        as="a"
-                        @click="logout" 
-                        class="
+                        <DisclosureButton as="a" @click="logout" class="
                         block px-3 py-2 rounded-md text-base font-medium 
                         text-gray-400 hover:text-white hover:bg-gray-700
                         cursor-pointer
-                        "                        
-                        >Sign out</DisclosureButton>
+                        ">Sign out</DisclosureButton>
                     </div>
                 </div>
             </DisclosurePanel>
@@ -118,8 +109,8 @@ import { computed } from 'vue';
 import { useRouter } from 'vue-router';
 
 const navigation = [
-    { name: 'Dashboard',  to: {name: 'Dashboard'} },
-    { name: 'Surveys', to: {name: 'Surveys'}},    
+    { name: 'Dashboard', to: { name: 'Dashboard' } },
+    { name: 'Surveys', to: { name: 'Surveys' } },
 ]
 export default {
     components: {
@@ -133,10 +124,13 @@ export default {
 
         function logout() {
             //sessionStorage.token = 
-            store.commit('logout');
-            router.push({
-                name: 'Login'
-            })
+            store.dispatch('logout')
+                .then(() => {
+                    router.push({
+                        name: 'Login'
+                    })
+                });
+
         }
 
         return {
