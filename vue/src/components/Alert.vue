e3a8e9e006045a9c5ba9c44bfb7d113356c6fba9
Guy Koumba <guykoumba@gmail.com>
2022-07-27 23:56:16 +0000

    err 02

---
+++ vue/src/components/Alert.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    v-if="errorMsg"
+    class="
+      flex
+      items-center
+      justify-between
+      py-3
+      px-5
+      bg-red-500
+      text-white
+      rounded
+    "
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
